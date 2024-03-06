2178f86fffef0f6442d707ace41d015aa565800c
Hao <Hao@MSI>
2024-03-06 15:08:52 +0700

    update

--- DemoGithub/Form1.Designer.cs
+++ DemoGithub/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(109, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(33, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "user";
             // 
             // textBox1
             // 
